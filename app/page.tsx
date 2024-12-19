f9223014a513df53a0bb3595845f5ecda861b1af
sujal-shukla <shuklasujal714@gmail.com>
2024-12-19 22:21:09 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mt-9 flex flex-col'>
+      <section className='flex flex-co gap-5l'>
+        <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+        <Button className='text-white-1 bg-orange-1'>Button </Button>
+      </section>
+    </div>
+  )
+}
+
+export default page
